bc77c1fdf5c2b06c13d1d82e0775dfc9e4126c54
Assam157 <asasm8106@gmail.com>
2025-01-28 11:41:41 +0530

    Update app.php

--- app.php
+++ app.php
@@ -483,6 +483,7 @@
      $data = $request->getParsedBody();
      $productId = $data['_id'];
      $newQuantity = $data['availableQuant'];
+     $newPrice=$data['price']
  
      try {
          $objectId = new MongoDB\BSON\ObjectId($productId);
@@ -493,7 +494,7 @@
      // Update product's available quantity
      $updateResult = $productCollection->updateOne(
          ['_id' => $objectId],
-         ['$set' => ['availableQuant' => $newQuantity]]
+         ['$set' => ['availableQuant' => $newQuantity,'price'=>$newPrice]]
      );
  
      if ($updateResult->getModifiedCount() > 0) {
